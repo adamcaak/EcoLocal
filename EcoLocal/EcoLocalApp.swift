32119f26a5406e141920e7b9c8a5d7ca613d2f9d
adamcaak <ladamczak1997@gmail.com>
2026-05-03 13:41:09 +0200

    Initial Commit

---
+++ EcoLocal/EcoLocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoLocalApp.swift
+//  EcoLocal
+//
+//  Created by Łukasz Adamczak on 03/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct EcoLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
