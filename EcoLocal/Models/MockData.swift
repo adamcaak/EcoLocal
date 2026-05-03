3ceef6ebe0df2a531f9e63d294ac3dd2090cab03
adamcaak <ladamczak1997@gmail.com>
2026-05-03 15:00:41 +0200

    Create file with fake data

---
+++ EcoLocal/Models/MockData.swift
@@ -0,0 +1,15 @@
+//
+//  MockData.swift
+//  EcoLocal
+//
+//  Created by Łukasz Adamczak on 03/05/2026.
+//
+
+import Foundation
+
+let mockVendors: [Vendor] = [
+    Vendor(name: "Eko Farma Kraków", category: "Warzywa", description: "Świeże warzywa od lokalnych rolników"),
+    Vendor(name: "Bio Nabiał Wieliczka", category: "Nabiał", description: "Naturalne mleko i sery"),
+    Vendor(name: "Zdrowe Pieczywo", category: "Pieczywo", description: "Chleb na zakwasie"),
+    Vendor(name: "Eco Miody", category: "Produkty naturalne", description: "Miody prosto z pasieki")
+]
