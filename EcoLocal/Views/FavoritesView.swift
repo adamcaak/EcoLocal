cf20a499ee5ff0fdcdec2cd5bbd4317b12f401a4
adamcaak <ladamczak1997@gmail.com>
2026-05-07 00:00:54 +0200

    Add empty state for favorites screen

--- EcoLocal/Views/FavoritesView.swift
+++ EcoLocal/Views/FavoritesView.swift
@@ -12,16 +12,27 @@ struct FavoritesView: View {
     @EnvironmentObject var viewModel: VendorViewModel
     
     var body: some View {
-        List (
-            viewModel.vendors.filter { $0.isFavorite }
-        ) { vendor in
-            NavigationLink {
-                VendorDetailView(vendor: vendor)
-            } label: {
-                Text(vendor.name)
+        
+        let favoriteVendors = viewModel.vendors.filter({ $0.isFavorite })
+        
+        if favoriteVendors.isEmpty {
+            ContentUnavailableView(
+                "Brak ulubionych",
+                systemImage: "heart.slash",
+                description: Text("Dodaj ulubionych sprzedawców aby widzieć ich tutaj!")
+            )
+        } else {
+            List (
+                viewModel.vendors.filter { $0.isFavorite }
+            ) { vendor in
+                NavigationLink {
+                    VendorDetailView(vendor: vendor)
+                } label: {
+                    Text(vendor.name)
+                }
             }
+            .navigationTitle("Ulubione")
         }
-        .navigationTitle("Ulubione")
     }
 }
 
